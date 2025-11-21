566c816f2935a83c58b4048a5f7c83e2b48f0bb9
Evan Chen <evan@evanchen.cc>
2025-11-21 12:37:31 -0500

    feat: paste in the code

---
+++ src/django_discordo/__init__.py
@@ -0,0 +1,22 @@
+"""
+django-discordo: Discord webhook handler for Django logging
+"""
+
+__version__ = "0.1.0"
+
+from .handler import ACTION_LOG_LEVEL as _ACTION_LOG_LEVEL
+from .handler import SUCCESS_LOG_LEVEL as _SUCCESS_LOG_LEVEL
+from .handler import VERBOSE_LOG_LEVEL as _VERBOSE_LOG_LEVEL
+from .handler import DiscordWebhookHandler as _DiscordWebhookHandler
+
+ACTION_LOG_LEVEL = _ACTION_LOG_LEVEL
+SUCCESS_LOG_LEVEL = _SUCCESS_LOG_LEVEL
+VERBOSE_LOG_LEVEL = _VERBOSE_LOG_LEVEL
+DiscordWebhookHandler = _DiscordWebhookHandler
+
+__all__ = [
+    "ACTION_LOG_LEVEL",
+    "SUCCESS_LOG_LEVEL",
+    "VERBOSE_LOG_LEVEL",
+    "DiscordWebhookHandler",
+]
